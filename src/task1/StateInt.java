df222c600cbdab935a7f54d9810afad89ee7cd73
ReedKat <goshaidze03@gmail.com>
2022-11-20 22:30:45 +0500

    Initial commit

---
+++ src/task1/StateInt.java
@@ -0,0 +1,17 @@
+package task1;
+
+public class StateInt {
+    private int value;
+
+    public StateInt(int value) {
+        this.value = value;
+    }
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized int increment() {
+        return value++;
+    }
+}
