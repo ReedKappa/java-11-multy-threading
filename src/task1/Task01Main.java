7654a2450a789fdb5e9fabfeaa8b835501bfa8cc
ReedKat <goshaidze03@gmail.com>
2022-11-21 10:32:59 +0500

    Done

--- src/task1/Task01Main.java
+++ src/task1/Task01Main.java
@@ -1,34 +1,34 @@
 package task1;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Task01Main {
-    static StateInt value = new StateInt(0);
-    static StateInt count = new StateInt(0);
-
-
+    static AtomicInteger x = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            int x;
-            while ((x = value.increment()) < 999999) {
-                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                    System.out.println(x);
-                    count.increment();
+            while (x.get() < 999999) {
+                int curX = x.incrementAndGet();
+                if (curX <= 999999 && ((curX % 10) + (curX / 10) % 10 + (curX / 100) % 10 == (curX / 1000)
+                        % 10 + (curX / 10000) % 10 + (curX / 100000) % 10)) {
+                    System.out.println(curX);
+                    count.incrementAndGet();
                 }
             }
         }
-    }
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new LuckyThread();
-        Thread t2 = new LuckyThread();
-        Thread t3 = new LuckyThread();
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
-        System.out.println("Total: " + count.get());
+        public static void main(String[] args) throws InterruptedException {
+            Thread t1 = new LuckyThread();
+            Thread t2 = new LuckyThread();
+            Thread t3 = new LuckyThread();
+            t1.start();
+            t2.start();
+            t3.start();
+            t1.join();
+            t2.join();
+            t3.join();
+            System.out.println("Total: " + count.get());
+        }
     }
 }
